c108fb8c0abd4370ef25a7b5e338296d9c6285f3
ukrgerri4 <ukrgerri4@gmail.com>
2021-01-11 12:37:02 +0200

    review

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 
 namespace _2_1
 {
+    // dsps
     class User
     {
         public string login;
@@ -9,13 +10,17 @@ namespace _2_1
         public string surname;
         public string age;
         private readonly string date;
-        
+
+        // этот метод не должен быть в классе User, т.к ты ничего из класса не используешь
+        // так же если ты изменишь Main как я написал ты его так не вызовешь как ты это делал
         static string GetInfoFromInput(string message)
         {
             Console.WriteLine(message);
             string newElement = Console.ReadLine();
             return newElement;
         }
+
+        // по условию вроде дату вводить не нужно, обычно дата это дата создания обьекта по умолчанию
         public User(string date)
         {
             this.date = date;
@@ -27,6 +32,8 @@ namespace _2_1
             Console.ReadKey();
         }
 
+
+        // этот метод обычно реализуется отдельно, а не в каком-то классе
         static void Main(string[] args)
         {
             string message = GetInfoFromInput("Введите дату:");
@@ -40,4 +47,23 @@ namespace _2_1
             user.OtputUserInfo();
         }
     }
+
+    // это плюс минус всегда должно быть так
+    /*
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = GetInfoFromInput("Введите дату:");
+            User user = new User(message);
+
+            user.login = GetInfoFromInput("Введите логин:");
+            user.name = GetInfoFromInput("Введите имя:");
+            user.surname = GetInfoFromInput("Введите фамилию:");
+            user.age = GetInfoFromInput("Введите свой возраст:");
+
+            user.OtputUserInfo();
+        }
+    }
+    */
 }

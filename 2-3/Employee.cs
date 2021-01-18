563a659b196a1b196c29f968c58f1124303df60f
Stanislav Skoryi <skoryistas@gmail.com>
2021-01-18 15:47:27 +0200

    Добавил домашку 2-3

---
+++ 2-3/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _2_4
+{
+    public class Employee
+    {
+        private string name;
+        private string surname;
+        private string position;
+        private double experience; //years
+        private const double TAX_RATE = 0.2; //20%
+        private double salary, taxes;
+
+        public Employee(string nameInput, string surnameInput, string positionInput, double experienceInput)
+        {
+            name = nameInput;
+            surname = surnameInput;
+            position = positionInput;
+            experience = experienceInput;
+        }
+
+        public void SetSalaryAndTaxes()
+        {
+            int initialRate = 0;
+            double experienceRate;
+
+            switch (position)
+            {
+                case "директор": initialRate = 1000; break;
+                case "охранник": initialRate = 400; break;
+                case "уборщик": initialRate = 300; break;
+                case "бухгалтер": initialRate = 600; break;
+                default: break;
+            }
+
+            experienceRate = experience < 1 ? 1 : experience < 3 ? 1.2 : 1.4;
+
+            salary = initialRate * experienceRate * (1 - TAX_RATE);
+
+            taxes = initialRate * experienceRate * TAX_RATE;
+        }
+
+        public void WriteUserInfoToConsole()
+        {
+            Console.WriteLine($"{name} {surname} занимает должность: {position}. Чистый оклад: {salary}. Налоговый сбор: {taxes}.");
+        }
+    }
+}
---
+++ 2-3/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Linq;
+
+namespace _2_4
+{
+    public class Program
+    {
+        public static string GetInfoFromInput(string message)
+        {
+            Console.WriteLine(message);
+            string newElement = Console.ReadLine();
+            return newElement;
+        }
+
+        public static double GetNumberFromInput(string message)
+        {
+            Console.WriteLine(message);
+            string newElementString = Console.ReadLine();
+            double newElementInt = Convert.ToDouble(newElementString);
+            return newElementInt;
+        }
+
+        public static string ValidationName(string inputInfo)
+        {
+            if (String.IsNullOrEmpty(inputInfo))
+            {
+                Console.WriteLine("Поле не должно быть пустым или пробелом.");
+                return null;
+            }
+            else if (!inputInfo.Any(char.IsLetter))
+            {
+                Console.WriteLine("Поле должно состоять из букв");
+                return null;
+            }
+            else
+            {
+                return inputInfo;
+            }
+        }
+
+        public static string ValidationPosition(string inputInfo)
+        {
+            if (String.IsNullOrEmpty(inputInfo))
+            {
+                Console.WriteLine("Поле не должно быть пустым или пробелом.");
+                return null;
+            }
+            else if (!inputInfo.Any(char.IsLetter))
+            {
+                Console.WriteLine("Поле должно состоять из букв");
+                return null;
+            }
+            else if (inputInfo == "директор" || inputInfo == "охранник" || inputInfo == "уборщик" || inputInfo == "бухгалтер")
+            {
+                return inputInfo;
+            }
+            else
+            {
+                Console.WriteLine("Не найдено совпадения с имеющимися должностями.");
+                return null;
+            }
+        }
+
+        public static double ValidationExperienceNumber(double inputNumber)
+        {
+            if (inputNumber == 0)
+            {
+                Console.WriteLine("Поле не должно быть нулем.");
+                return 0;
+            }
+            else if (Double.IsNegativeInfinity(inputNumber))
+            {
+                Console.WriteLine("Поле не должно содержать отрицательных цифр.");
+                return 0;
+            }
+            else
+            {
+                return inputNumber;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string nameInput = null, surnameInput = null, positionInput = null;
+            double experienceInput = 0;
+
+            while (nameInput == null)
+            {
+                nameInput = GetInfoFromInput("Введите имя:");
+                nameInput = ValidationName(nameInput);
+            }
+
+            while (surnameInput == null)
+            {
+                surnameInput = GetInfoFromInput("Введите фамилию:");
+                surnameInput = ValidationName(surnameInput);
+            }
+
+            while (positionInput == null)
+            {
+                positionInput = GetInfoFromInput("Введите должность:");
+                positionInput = ValidationPosition(positionInput);
+            }
+
+            while (experienceInput == 0)
+            {
+                experienceInput = GetNumberFromInput("Введите опыт работы в годах:");
+                experienceInput = ValidationExperienceNumber(experienceInput);
+            }
+
+            Employee employee = new Employee(nameInput, surnameInput, positionInput, experienceInput);
+
+            employee.SetSalaryAndTaxes();
+            employee.WriteUserInfoToConsole();
+
+            Console.ReadKey(); //Delay
+        }
+    }
+}
